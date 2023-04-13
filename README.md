658e479c5f0d59832d4f06df1f5d56f61a95f259
henriquepercudeoliveira <henrique.percu@gmail.com>
2023-04-13 19:50:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## About the project
+
+This project is a simple demo for a container application with a pipeline to be deployed in AWS 
+
+## Stack
+
+Java
+
+Sprint Boot
+
+Terraform
+
+## Pipeline
+
+In order to deploy, the pipeline used follow this workflow: GitHub Actions -> Terraform Cloud -> AWS.
---
+++ deployment/main.tf
@@ -0,0 +1,36 @@
+resource "aws_ecs_task_definition" "demo_container_application_task_definition" {
+
+  family                   = var.aws_ecs_task_def_fam
+  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  container_definitions = jsonencode([
+    {
+      name   = var.aws_ecr_repository
+      image  = "${local.account_id}.dkr.ecr.${local.region_name}.amazonaws.com/<<container_name>>:latest" // parametrizar essa informacao
+      cpu    = var.fargate_cpu
+      memory = var.fargate_memory
+      portMappings = [
+        {
+          containerPort = var.app_port
+          hostPort      = var.app_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "main" {
+  name            = var.service_name
+  cluster         = data.aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.demo_container_application_task_definition.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  depends_on = [
+    aws_ecs_task_definition.demo_container_application_task_definition,
+  ]
+
+}
